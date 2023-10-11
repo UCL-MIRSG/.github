99d04494fe2fa2f4d777674ed6b76058f11d817a
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-11 15:51:26 +0100

    Try bool

--- precommit/run-mirsg-hooks.py
+++ precommit/run-mirsg-hooks.py
@@ -6,11 +6,12 @@ import sys
 HERE = pathlib.Path(__file__).resolve()
 
 
-def main() -> None:
-    print(HERE.parent)
-    cfg = HERE.parent / ".pre-commit-config.yaml"
-    cmd = ["pre-commit", "run", "--config", f"{cfg}", "--files"] + sys.argv[1:]
-    subprocess.run(cmd)
+def main(run_once: int = 0) -> None:
+    if not run_once:
+        cfg = HERE.parent / ".pre-commit-config.yaml"
+        cmd = ["pre-commit", "run", "--config", f"{cfg}", "--files"] + sys.argv[1:]
+        subprocess.run(cmd)
+        run_once += 1
 
 
 if __name__ == "__main__":
