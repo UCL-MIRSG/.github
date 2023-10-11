23a4b4e50578ad49ef612e956a12231de7785994
Patrick Roddy <patrickjamesroddy@gmail.com>
2023-10-11 14:23:19 +0100

    Default `pre-commit` hooks? (#3)
    Let's give it a go?

---
+++ precommit/run-mirsg-hooks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import pathlib
+import subprocess
+import sys
+
+HERE = pathlib.Path(__file__).resolve()
+
+
+def main() -> None:
+    cfg = HERE.parent / ".pre-commit-config.yaml"
+    cmd = ["pre-commit", "run", "--config", f"{cfg}", "--files"] + sys.argv[1:]
+    subprocess.run(cmd)
+
+
+if __name__ == "__main__":
+    exit(main())
