1be552e8adeb16e7b1ac539b9f92741d43dd109c
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-11 15:13:36 +0100

    Restore example from stackoverflow

--- precommit/run-mirsg-hooks.py
+++ precommit/run-mirsg-hooks.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
-import pathlib
-import subprocess
+import os
 import sys
 
-HERE = pathlib.Path(__file__).resolve()
+HERE = os.path.dirname(os.path.realpath(__file__))
 
 
-def main() -> None:
-    cfg = HERE.parent / ".pre-commit-config.yaml"
-    cmd = ["pre-commit", "run", "--config", f"{cfg}", "--files"] + sys.argv[1:]
-    subprocess.run(cmd)
+def main():
+    cfg = os.path.join(HERE, ".pre-commit-config.yaml")
+    cmd = ["pre-commit", "run", "--config", cfg, "--files"] + sys.argv[1:]
+    os.execvp(cmd[0], cmd)
 
 
 if __name__ == "__main__":
-    main()
+    exit(main())
